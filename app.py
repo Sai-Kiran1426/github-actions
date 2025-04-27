0ff97aba75c7ec16310b0f03ff791936200e5ec7
Sai-Kiran1426 <saikiran.m9498@gmail.com>
2025-04-27 16:02:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask
-import os
 app = Flask(_name_)
 
 @app.route('/')
@@ -7,6 +6,5 @@ def hello_world():
     return 'Hello hai!'
 
 if _name_ == '_main_':
-    # Ensuring the app runs on the correct port
-    port = int(os.environ.get('PORT', 8080))
-    app.run(host='0.0.0.0', port=port)
+   
+    app.run(host='0.0.0.0', port=8080)
